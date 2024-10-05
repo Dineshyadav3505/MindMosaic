9caade65633b8e822dd02ec75797d4ae3022b83a
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-10-05 22:50:44 +0530

    chore: Add tweet app and update project settings and URLs

--- MindMosaic/MindMosaic/settings.py
+++ MindMosaic/MindMosaic/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles', 
+    'tweet',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'MindMosaic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- MindMosaic/tweet/apps.py
+++ MindMosaic/tweet/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ChaiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chai'
+    name = 'tweet'
